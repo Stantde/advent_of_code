8d37647b477b654b073a7d8a7d61cef36e2b32f4
Spectre050287 <d.stanton256@gmail.com>
2025-12-06 14:58:33 -0600

    stuck again

--- 2025/6_trash_compactor/2_part_solution.py
+++ 2025/6_trash_compactor/2_part_solution.py
@@ -19,10 +19,17 @@ def get_file_data(): # returns dict with key=line_number,value=line_text
     count =0
     with open(filename,'r') as f:
         for line in f:
-            foo[count]=[fvalue for fvalue in line.strip('\n').split(' ') if fvalue !='']
-            if foo[count][0] in ['+','*']:
+            foo_line=[]
+            #foo[count]=[]
+            MAGIC_NUMBER=5
+            tmp=3785/MAGIC_NUMBER
+            for i in range(int(3785/MAGIC_NUMBER)):
+                foo_line.append(line[i*MAGIC_NUMBER:i*MAGIC_NUMBER+MAGIC_NUMBER-1])
+            foo[count]=foo_line
+            if foo[count][0].strip() in ['+','*']:
                 foo['sign']= foo[count]
                 foo.pop(count, None)
+            tmp=len(line)
             count+=1
 
     return foo 
@@ -42,15 +49,53 @@ def get_sum(homework_set):
     grand_total=0    
     try:
         for pset in range(len(homework_set[0])):
-            sign = homework_set['sign'][pset]
+            sign = homework_set['sign'][pset].strip()
             total=0
+            unorganized_terms=[]
+            organized_terms=[]
             for key in range(len(homework_set.keys())-1):
-                if total==0:
+                unorganized_terms.append(homework_set[key][pset])
+                """if total==0:
                     total=int(homework_set[key][pset])
                 elif sign=='+':
                     total+=int(homework_set[key][pset])                    
                 elif sign=='*':
-                    total*=int(homework_set[key][pset])
+                    total*=int(homework_set[key][pset])"""
+            max_length=0
+            for term in unorganized_terms:
+                if max_length == 0:
+                    max_length=len(term)
+                if len(term)>max_length:
+                    max_length=len(term)
+            baz=[]
+            not_empty=True
+            while not_empty:                
+                not_empty=False
+                digit=1
+                i=0
+                new_num=0
+                for term in unorganized_terms: # ['123', '45', '6'] for example sign =*, expect 356*24*1=8544
+                    if term:
+                        try:
+                            num=int(term[-1])%10
+                        except ValueError:
+                            num=0
+                        unorganized_terms[i]=unorganized_terms[i][:len(term)-1] #expect one less 
+                        not_empty=True
+                    else: 
+                        num=0
+                    new_num+=digit*num
+                    digit*=10
+                    i+=1
+                if int(str(new_num)[::-1])>0:
+                    baz.append(int(str(new_num)[::-1]))
+            for term in baz:
+                if total==0:
+                    total=term
+                elif sign=='+':
+                    total+=term
+                elif sign=='*':
+                    total*=term
             grand_total+=total
     except KeyError:
             pass
